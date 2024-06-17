436cd401eddf0790394ef24517158015a141b18b
SilvieHong <122876238+SilvieHong@users.noreply.github.com>
2024-06-17 10:24:36 +0900

    Create 의상.java

---
+++ Lv.2/의상.java
@@ -0,0 +1,15 @@
+//https://school.programmers.co.kr/learn/courses/30/lessons/42578?language=java
+import java.util.*;
+class Solution {
+    public int solution(String[][] clothes) {
+        int answer = 1;
+        HashMap<String, Integer> map = new HashMap<>();
+        for (int i = 0; i < clothes.length; i++) {
+            map.put(clothes[i][1], map.getOrDefault(clothes[i][1], 0) + 1);
+        }
+        for (String key : map.keySet()) {
+            answer *= map.get(key) + 1;
+        }
+        return answer - 1;
+    }
+}
