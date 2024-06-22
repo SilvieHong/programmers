17b45769972e10746c70c8739c0770006ea75fd1
SilvieHong <122876238+SilvieHong@users.noreply.github.com>
2024-06-23 08:59:20 +0900

    Update 시저_암호.kt

--- Lv.1/시저_암호.kt
+++ Lv.1/시저_암호.kt
@@ -3,10 +3,18 @@ class Solution {
         var answer = ""
 
         for (element in s) {
-            if (element.isLowerCase()) {
-                answer += ((element.toInt() + n) % 26 + 78).toChar()
-            } else if (element.isUpperCase()) {
-                answer += ((element.toInt() + n) % 26 + 52).toChar()
+            if (element.toInt() in 97..122) {
+                if(element.toInt()+n >122){
+                    answer += (element.toInt()+n-26).toChar()
+                }else{
+                    answer += (element.toInt() + n).toChar()
+                }
+            } else if (element.toInt() in 65..90) {
+                if(element.toInt()+n >90){
+                    answer += (element.toInt()+n-26).toChar()
+                }else{
+                    answer += (element.toInt() + n).toChar()
+                }
             } else {
                 answer += " "
             }
