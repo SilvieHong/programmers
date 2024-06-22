76948c52b1872d4cdf36927bec4658a70d5e0bd7
SilvieHong <122876238+SilvieHong@users.noreply.github.com>
2024-06-22 16:56:23 +0900

    Create 나누어_떨어지는_숫자_배열.kt

---
+++ Lv.1/나누어_떨어지는_숫자_배열.kt
@@ -0,0 +1,10 @@
+class Solution {
+    fun solution(arr: IntArray, divisor: Int): IntArray {
+        val answer = mutableListOf<Int>()
+        for(i in arr){
+            if(i%divisor==0) answer.add(i)
+        }
+        if(answer.isEmpty()) answer.add(-1)
+        return answer.sorted().toIntArray()
+    }
+}
