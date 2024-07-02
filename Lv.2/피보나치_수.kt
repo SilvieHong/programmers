5321b8dd502128f4a157e4860e2829f80ce57309
SilvieHong <122876238+SilvieHong@users.noreply.github.com>
2024-07-02 22:34:12 +0900

    Create 피보나치_수.kt

---
+++ Lv.2/피보나치_수.kt
@@ -0,0 +1,13 @@
+class Solution {
+    fun solution(n: Int): Int {
+        val fibonacci = mutableListOf<Int>()
+        fibonacci.add(0)
+        fibonacci.add(1)
+
+        for (i in 2..100000) {
+            fibonacci.add((fibonacci[i - 1] + fibonacci[i - 2])%1234567)
+            if (i == n) return fibonacci[i]
+        }
+        return 0;
+    }
+}
