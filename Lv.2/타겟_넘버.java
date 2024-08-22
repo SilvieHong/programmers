ea5a5551a1d73b0b4688d6fe778f42058a4d1273
SilvieHong <122876238+SilvieHong@users.noreply.github.com>
2024-08-22 21:02:00 +0900

    Create 타겟_넘버.java

---
+++ Lv.2/타겟_넘버.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int solution(int[] numbers, int target) {
+        return dfs(numbers, 0, 0, target);
+    }
+    
+    private static int dfs(int[] numbers, int idx, int sum, int target) {
+        if (idx == numbers.length) return sum == target ? 1 : 0;
+
+        return dfs(numbers, idx + 1, sum + numbers[idx], target)
+                + dfs(numbers, idx + 1, sum - numbers[idx], target);
+    }
+}
